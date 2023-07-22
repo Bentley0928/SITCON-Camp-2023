b7134e25877ca98c1c034019b3c008b82726c840
Bentley <bentleyhuang@BentleyMacBook-Pro-14.local>
2023-07-22 14:52:15 +0800

    add

--- 爬蟲/cat-image.py
+++ 爬蟲/cat-image.py
@@ -7,7 +7,7 @@ url = "https://api.thecatapi.com/v1/images/search"
 for ii in range(0,i):
     response = requests.get(url)
     data = json.loads(response.text)
-    urls = [d["url"] for d in data]
+    urls = data[0]["url"]
     with open("result.json", "a") as f:
         f.write("\n".join(urls) + "\n")
 
